2032cad39aa523c2c256423689b5804a7e72ee71
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2020-12-30 23:54:59 +0100

    adding files

---
+++ multidimensional arrays/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace multidimensional_arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResultaat = new System.Windows.Forms.TextBox();
+            this.btnPrint1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResultaat
+            // 
+            this.txtResultaat.Location = new System.Drawing.Point(80, 99);
+            this.txtResultaat.Multiline = true;
+            this.txtResultaat.Name = "txtResultaat";
+            this.txtResultaat.Size = new System.Drawing.Size(571, 291);
+            this.txtResultaat.TabIndex = 0;
+            // 
+            // btnPrint1
+            // 
+            this.btnPrint1.Location = new System.Drawing.Point(243, 29);
+            this.btnPrint1.Name = "btnPrint1";
+            this.btnPrint1.Size = new System.Drawing.Size(281, 51);
+            this.btnPrint1.TabIndex = 1;
+            this.btnPrint1.Text = "Print";
+            this.btnPrint1.UseVisualStyleBackColor = true;
+            this.btnPrint1.Click += new System.EventHandler(this.btnPrint1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrint1);
+            this.Controls.Add(this.txtResultaat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResultaat;
+        private System.Windows.Forms.Button btnPrint1;
+    }
+}
+
---
+++ multidimensional arrays/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multidimensional_arrays
+{
+    public partial class Form1 : Form
+    {
+       
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint1_Click(object sender, EventArgs e)
+        {
+            Random myRandom = new Random();
+            myRandom.Next(0, 101);
+            int total = 0;
+            int[,] myarray = new int[5, 3];
+            txtResultaat.Multiline = true;
+            for (int i = 0; i < 5; i++)
+            {
+
+                myarray[i, 0] = myRandom.Next(0, 100);
+                myarray[i, 1] = myRandom.Next(0, 100);
+                myarray[i, 2] = myarray[i, 0] + myarray[i, 1];
+                total = myarray[i, 2] + total;
+
+
+            }
+            printArray(myarray);
+      
+            
+           
+        }
+
+        private void printArray (int[,] arrayToPrint)
+        {
+            txtResultaat.Text = "";
+            for(int iRow =0; iRow < arrayToPrint.GetLowerBound(0) + 3;iRow++)
+            {
+                for(int iColumn=0; iColumn < arrayToPrint.GetUpperBound(1) +1; iColumn++)
+                {
+                    txtResultaat.Text = txtResultaat.Text + arrayToPrint[iRow, iColumn].ToString() + "\t";
+
+                }
+                txtResultaat.Text = txtResultaat.Text + Environment.NewLine + Environment.NewLine;
+            }
+
+        }
+    }
+}
